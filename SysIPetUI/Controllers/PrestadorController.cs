d396ec075abe17432d212e6efdea5cef385dae86
Edu Carvalho <educarvalho.tec@gmail.com>
2022-08-26 20:23:08 -0300

    PrestadorController: - Adicionada Rotina ExcluirPrestador; - Adicionada Rotina EditPrestador; Views\Prestador: - Novas Views de Modal para Excluir e Editar; - Novos links nos botões Excluir e Editar na View Index;

--- SysIPetUI/Controllers/PrestadorController.cs
+++ SysIPetUI/Controllers/PrestadorController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using SysIPetUI.Models;
+using System.Net.Http.Json;
 using System.Text;
 
 namespace SysIPetUI.Controllers
@@ -47,12 +48,6 @@ namespace SysIPetUI.Controllers
             }
         }
 
-        // GET: PrestadorController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
         // GET: PrestadorController/Create
         public ActionResult CreatePrestador()
         {
@@ -64,14 +59,17 @@ namespace SysIPetUI.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> CreatePrestador(PrestadorViewModel prestador)
         {
-            PrestadorViewModel? prestadorIncluido = new PrestadorViewModel();
-
             try
             {
+                PrestadorViewModel? prestadorIncluido = new PrestadorViewModel();
+
                 using (var httpClient = new HttpClient())
                 {
+                    //Como a API precisará dos novos dados do prestador no formato JSON, estamos serializando os dados
+                    //da ViewModel PrestadorViewModel para JSON e depois convertendo-os em um objeto StringContent:
                     StringContent content = new StringContent(JsonConvert.SerializeObject(prestador), Encoding.UTF8, "application/json");
 
+                    //Aqui realizamos o PostAsync que Utiliza a Pet.WebAPI para inserir um novo Prestador na Tabela do SQL
                     using (var response = await httpClient.PostAsync(url, content))
                     {
                         string responseBody = await response.Content.ReadAsStringAsync();
@@ -88,45 +86,82 @@ namespace SysIPetUI.Controllers
         }
 
         // GET: PrestadorController/Edit/5
-        public ActionResult Edit(int id)
+        [HttpGet]
+        public async Task<IActionResult> EditPrestador(int Id)
         {
-            return View();
+            PrestadorViewModel? prestador = new PrestadorViewModel();
+
+            using (var httpClient = new HttpClient())
+            {
+                //Aqui realizamos o GetAsync que Utiliza a Pet.WebAPI para Retornar o Prestador que será Editado pelo Id na Tabela do SQL
+                //Dessa forma exibimos no Modal os Detalhes do Prestador que será Editado.
+                using (var response = await httpClient.GetAsync(url + "/" + Id))
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    prestador = JsonConvert.DeserializeObject<PrestadorViewModel>(responseBody);
+                }
+            }
+            return View(prestador);
         }
 
         // POST: PrestadorController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<IActionResult> EditPrestador(PrestadorViewModel prestador)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                PrestadorViewModel? prestadorRecebido = new PrestadorViewModel();
+
+                using (var httpClient = new HttpClient())
+                {
+                    //Aqui realizamos o PutAsync que Utiliza a Pet.WebAPI para Editar o Prestador na Tabela do SQL usando o Id
+                    using (var response = await httpClient.PutAsJsonAsync($"{url}/{prestador.Id}", prestador))
+                    {
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        prestadorRecebido = JsonConvert.DeserializeObject<PrestadorViewModel>(responseBody);
+                    }
+                }
+                return RedirectToAction("Index");
             }
-            catch
+            catch (Exception)
             {
-                return View();
+                return View("Error");
             }
         }
 
-        // GET: PrestadorController/Delete/5
-        public ActionResult Delete(int id)
+        // GET: PrestadorController/ExcluirPrestador/5
+        public async Task<IActionResult> ExcluirPrestador(int Id)
         {
-            return View();
+            PrestadorViewModel? prestador = new PrestadorViewModel();
+
+            using (var httpClient = new HttpClient())
+            {
+                //Aqui realizamos o GetAsync que Utiliza a Pet.WebAPI para Retornar o Prestador que será excluído pelo Id na Tabela do SQL
+                //Dessa forma exibimos no Modal os Detalhes do Prestador que será excluído.
+                using (var response = await httpClient.GetAsync(url + "/" + Id))
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    prestador = JsonConvert.DeserializeObject<PrestadorViewModel>(responseBody);
+                }
+            }
+            return View(prestador);
         }
 
-        // POST: PrestadorController/Delete/5
+        // POST: PrestadorController/ExcluirPrestador/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<IActionResult> ExcluirPrestador(int Id, IFormCollection form)
         {
-            try
+            using (var httpClient = new HttpClient())
             {
-                return RedirectToAction(nameof(Index));
+                //Aqui realizamos o DeletAsync que Utiliza a Pet.WebAPI para Deletar o Prestador na Tabela do SQL usando o Id
+                using (var response = await httpClient.DeleteAsync(url + "/" + Id))
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                }
             }
-            catch
-            {
-                return View();
-            }
+            return RedirectToAction("Index");
         }
     }
 }
