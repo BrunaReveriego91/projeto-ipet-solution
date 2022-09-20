e2a39fe5647dad9559527954ac7cbe6edf80389b
Bruna Reveriego <Bruna.Reveriego@br.ey.com>
2022-09-20 17:41:39 -0300

    alteracao service prestador teste unitario

--- Pet.UnitTests/Services/PrestadoresServiceTestes.cs
+++ Pet.UnitTests/Services/PrestadoresServiceTestes.cs
@@ -1,7 +1,7 @@
 ﻿using NSubstitute;
 using Pet.WebAPI.Domain.Entities;
+using Pet.WebAPI.Domain.Model;
 using Pet.WebAPI.Interfaces.Repositories;
-using Pet.WebAPI.Interfaces.Services;
 using Pet.WebAPI.Services;
 using Xunit;
 
@@ -28,47 +28,53 @@ namespace Pet.UnitTests.Services
             var service = CriaServico();
             //Act
             var enderecoPrestador = new EnderecoPrestador() { Logradouro = "Rua do Teste", Bairro = "Vila do Teste", CEP = "0000-000", Cidade = "São Paulo", Numero = 1, PrestadorId = 1, Complemento = "N/A", Data_Cadastro = new System.DateTime(2015, 3, 10), Referencia = "Esquina do Teste", SemNumero = false, UF = "SP", Telefone = "(11)1234-56789", WhatsApp = true };
+            var servicoPrestador = new ServicoPrestador() { Id = 1, PrestadorId = 1, ServicoId = 1, Ativo = true, Data_Cadastro = new System.DateTime(2015, 01, 01), Valor = 50.00f };
 
             var listEndereco = new List<EnderecoPrestador>() { enderecoPrestador };
+            var listServico = new List<ServicoPrestador>() { servicoPrestador };
 
-            var prestador = new Prestador() { Id = 1, NomeCompleto = "Fulano de Tal", CPF_CNPJ = "111.111.111-111", Data_Cadastro = new System.DateTime(2015, 3, 10), Telefone = "(11) 1234-5678", WhatsApp = true,Enderecos = listEndereco };
+
+            var prestador = new Prestador() { Id = 1, NomeCompleto = "Fulano de Tal", CPF_CNPJ = "111.111.111-111", Data_Cadastro = new System.DateTime(2015, 3, 10), Telefone = "(11) 1234-5678", WhatsApp = true, Enderecos = listEndereco, Servicos = listServico };
             _prestadoresRepository.Get(1).Returns(prestador);
 
             var resultado = service.Get(1);
 
             //Assert
             Assert.NotNull(resultado);
-            Assert.IsType<Cliente>(resultado);
+            Assert.IsType<Prestador>(resultado);
         }
 
-        //[Fact]
-        //public async Task InserirNovoClienteAsync()
-        //{
-        //    //Arrange
-        //    var service = CriaServico();
-        //    //Act
-        //    var cliente = new Cliente() { Id = 1, NomeCompleto = "Fulano de Tal", Aniversario = "01/01/1001", CPF = "123.456.789-10", WhatsApp = true, Telefone1 = "(11)99999-9999", Telefone2 = "(22)88888-8888" };
+        [Fact]
+        public async Task InserirNovoPrestadorAsync()
+        {
+            //Arrange
+            var service = CriaServico();
+            //Act
+            var enderecoPrestador = new EnderecoPrestador() { Logradouro = "Rua do Teste", Bairro = "Vila do Teste", CEP = "0000-000", Cidade = "São Paulo", Numero = 1, PrestadorId = 1, Complemento = "N/A", Data_Cadastro = new System.DateTime(2015, 3, 10), Referencia = "Esquina do Teste", SemNumero = false, UF = "SP", Telefone = "(11)1234-56789", WhatsApp = true };
+            var servicoPrestador = new ServicoPrestador() { Id = 1, PrestadorId = 1, ServicoId = 1, Ativo = true, Data_Cadastro = new System.DateTime(2015, 01, 01), Valor = 50.00f };
+
+            var listEndereco = new List<EnderecoPrestador>() { enderecoPrestador };
+            var listServico = new List<ServicoPrestador>() { servicoPrestador };
 
 
-        //    _clienteRepository.Add(Arg.Any<Cliente>()).Returns(Task.FromResult<Cliente>(cliente));
-
-        //    var novoCliente = new NovoCliente()
-        //    {
-        //        NomeCompleto = cliente.NomeCompleto,
-        //        CPF = cliente.CPF,
-        //        Aniversario = cliente.Aniversario,
-        //        Telefone1 = cliente.Telefone1,
-        //        WhatsApp = cliente.WhatsApp,
-        //        Telefone2 = cliente.Telefone2
-        //    };
+            var prestador = new Prestador() { Id = 1, NomeCompleto = "Fulano de Tal", CPF_CNPJ = "123.456.789-10", WhatsApp = true, Telefone = "(11)99999-9999", Data_Cadastro = new DateTime(2015, 01, 01), Enderecos = listEndereco, Servicos = listServico };
 
 
-        //    var resultado = await service.Add(novoCliente);
+            _prestadoresRepository.Add(Arg.Any<Prestador>()).Returns(Task.FromResult<Prestador>(prestador));
 
-        //    //Assert
-        //    Assert.NotNull(resultado);
-        //    Assert.IsType<Cliente>(resultado);
-        //}
+            var novoPrestador = new NovoPrestador()
+            {
+                NomeCompleto = "Fulano de Tal",
+                CPF_CNPJ = "123.456.789-10"
+            };
+
+
+            var resultado = await service.Add(novoPrestador);
+
+            //Assert
+            Assert.NotNull(resultado);
+            Assert.IsType<Prestador>(resultado);
+        }
 
 
     }
